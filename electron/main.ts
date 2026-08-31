961016671bd1d36a8a7bbe58fab2cf55b7e6466c
Ângelo Cardoso <hoanngelos@gmail.com>
2026-08-31 00:16:02 -0300

    Add Electron desktop integration.

---
+++ electron/main.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    resizable: true,
+    autoHideMenuBar: true,
+
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
